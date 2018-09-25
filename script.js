636e34ae2bacfcdfea49f97c458c2ef191882d2a
psadovski <pawelsadowski92@gmail.com>
2018-09-25 18:34:45 +0200

    syles.css and script.js changed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   const apiRoot = 'https://limitless-retreat-17162.herokuapp.com/v1/task/';
   const trelloApiRoot = 'https://limitless-retreat-17162.herokuapp.com/v1/trello/';
-  const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
+ const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
   var availableBoards = {};
