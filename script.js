1c3805575f07bed3ca9091fcc5393318e3456575
psadovski <pawelsadowski92@gmail.com>
2018-10-13 12:48:20 +0200

    script changed

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ $(document).ready(function() {
     var requestUrl = apiRoot + 'delete';
 
     $.ajax({
-      url: requestUrl + '/' + $.param({
+      url: requestUrl + '?' + $.param({
         taskId: taskId
       }),
       method: 'DELETE',
